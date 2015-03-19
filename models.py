6d0fedec155794b2b6c2af497016a6a24c47338f
dev_brian_cho <kwang8992@gmail.com>
2015-03-19 18:29:26 +0900

    test를 위하여 User 테이블을 만듬. 추후 DB설계시 고려해야할 사항들(외래키, 프라이머리키, 데이터 사이즈 등을 고려하지 않고 만듬)

---
+++ models.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/secondTest.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    account = db.Column(db.String(100))
+    recentUrl = db.Column(db.String(100))
+    word = db.Column(db.String(100))
+    mean = db.Column(db.String(100))
+    deleteCheck = db.Column(db.String(2)) # default = N
+
+    # username = db.Column(db.String(80), unique=True)
+    # email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, account, recentUrl, word, mean, deleteCheck):
+        self.account = account
+        self.recentUrl = recentUrl
+        self.word = word
+        self.mean = mean
+        self.deleteCheck = deleteCheck
+
+    def __repr__(self):
+        return '<User %r>' % self.account
