c98ce93fb2e37172829202839ef94fb760d52b9c
dev_brian_cho <kwang8992@gmail.com>
2015-03-20 00:03:24 +0900

    한글 인코딩 & dataControl로 insertData, referenceData, updateData모듈 이동

--- server.py
+++ server.py
@@ -1,38 +1,20 @@
-from models import *
+# -*- coding:utf-8 -*-
+
+from dataControl import *
 
 db.create_all()
 
 @app.route('/dataControl/insertData')
-def insertData(account, recentUrl, word, mean, deleteCheck):
-	account = account
-	recentUrl = recentUrl
-	word = word
-	mean = mean
-	deleteCheck = deleteCheck
+def handlerInsertData(account, recentUrl, word, mean, deleteCheck):
+	insertData(account, recentUrl, word, mean, deleteCheck)
 
-
-	namnam = User(account, recentUrl, word, mean, deleteCheck)
-
-	try:
-		db.session.add(namnam)
-		db.session.commit()
-	except:
-		db.session.rollback()
-	finally:
-		db.session.close()
-
-	users = User.query.all()
-	# admin = User.query.filter_by(username='haha').all()
-	select = User.query.filter_by(account = 'd').all()
-	print users
-
-	print '====================='
-	print select
+@app.route('/dataControl/reference')
+def handlerReferenceData(account, url):
+	referenceData(account, url)
 
 @app.route('/dataControl/updateData')
-def updateData():
-
-	return users
+def handlerUpdateData(account, word):
+	updateData(account, word)
 
 if __name__ == '__main__':
 	app.run()
