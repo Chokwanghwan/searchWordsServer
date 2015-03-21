d780b525021cebb1dfaf69dbca4833be7d378c4e
dev_brian_cho <kwang8992@gmail.com>
2015-03-22 01:28:34 +0900

    인자로 넘어온값의 DB존재 여부를 파악한 후 특정 조건을 만족하면 DB의 데이터를 출력하는 showData()모듈 제작

--- dataControl.py
+++ dataControl.py
@@ -9,7 +9,6 @@ def insertData(account, recentUrl, word, mean, deleteCheck):
 	mean = mean
 	deleteCheck = deleteCheck
 
-
 	namnam = User(account, recentUrl, word, mean, deleteCheck)
 
 	try:
@@ -22,27 +21,33 @@ def insertData(account, recentUrl, word, mean, deleteCheck):
 
 	users = User.query.all()
 	# admin = User.query.filter_by(username='haha').all()
-	select = User.query.filter_by(account = 'd').all()
 	print users
 
-	print '====================='
-	print select
-	print '====================='
+	print '============================'
 
+def showData(account, reqUrl):
+	#테스트를 위해서 그냥 account과 url모두가 일치하고 deleteCheck가 'N'인 
+	#놈의 word와 mean을 출력할것. 원래라면 클라이언트에 보내줘야함.
+	account = account
+	reqUrl = reqUrl
+	
+	datas = User.query.filter_by(account=account, recentUrl=reqUrl, deleteCheck='N').all()
+	for i in datas:
+		print i.word + ' : ' + i.mean
+
+ 
 def updateData(account, word):
 	account = account
 	word = word
 	#현재 사용자가 삭제요청을 한 단어를받아서 deleteCheck를 수정한다.
 	db.session.query(User).update({Stuff.foo: Stuff.foo + 1})
 	db.session.commit()
-	return users		
+	return users
 
-def referenceData(account, reqUrl):
-	account = account
-	reqUrl = reqUrl
-	print account
-	print reqUrl
-	#현재 사용자가 현재 방문중인 url에서 어떤 단어들을 가장 원하는지 찾아준다
-	showInfo = User.query.filter_by(account=account, recentUrl=reqUrl).all()
-	# show = User.query.all()
-	print showInfo
+
+
+
+
+
+
+
