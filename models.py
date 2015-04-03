57bb5f70ef8686bcd9a20cd4cd6ff2d83c3d43ea
dev_brian_cho <kwang8992@gmail.com>
2015-04-03 17:23:29 +0900

    서비스에 적합한 DB 설계. table 구성 완료. User table에 데이터를 insert하는 class method 추가.

--- models.py
+++ models.py
@@ -1,78 +1,127 @@
-# -*- coding:utf-8 -*-
-
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/secondTest.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
-"""
-*   ==> 1:M
-**  ==> M:M
-User
-    id
-    email
-
-    *oauth
-    *urls
-    *word_book
-
-WordBook
-    id
-    word_id
-    user_id
-    *urls
-
-Word
-    id
-    english
-    mean
-
-    **urls
-
-Url
-    id
-    link
-
-    **words
-"""
-
+urls = db.Table('urls',
+    db.Column('url_id', db.Integer, db.ForeignKey('url.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    account = db.Column(db.String(100))
-    recentUrl = db.Column(db.String(100))
-    word = db.Column(db.String(100))
-    mean = db.Column(db.String(100))
-    deleteCheck = db.Column(db.String(2)) # default = N
+    email = db.Column(db.String(45))
+    # password = db.Column(db.String(32))
 
-    # username = db.Column(db.String(80), unique=True)
-    # email = db.Column(db.String(120), unique=True)
+    word_books = db.relationship('WordBook', backref='user',
+                                lazy='dynamic')
+    urls = db.relationship('Url', secondary=urls,
+        backref=db.backref('users', lazy='dynamic'))
+    
+    def __init__(self, email):
+        self.email = email
 
-    def __init__(self, account, recentUrl, word, mean, deleteCheck):
-        self.account = account
-        self.recentUrl = recentUrl
-        self.word = word
-        self.mean = mean
-        self.deleteCheck = deleteCheck
+    @classmethod
+    def fuck_up(cls, email):
+        user = cls.find_by_email(email)
+        if user is None:
+            cls.insert_user(email)
+        inserted_user = cls.find_by_email(email)
+        return inserted_user
 
-    def __repr__(self):
-        return '<User %r>' % self.account
+    @classmethod
+    def insert_user(cls, email):
+        u = User(email)
+        db.session.add(u)
+        db.session.commit()
+
+    @classmethod
+    def find_by_email(cls, email):
+        user = cls.query.filter_by(email=email).first()
+        return user
+
+class WordBook(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    word_id = db.Column(db.Integer, db.ForeignKey('word.id'))
+    refer_urls = db.relationship('ReferUrl', backref='word_book',
+                                lazy='dynamic')
 
 words = db.Table('words',
-    db.Column('url_id', db.Integer, db.ForeignKey('url.id')),
-    db.Column('word_id', db.Integer, db.ForeignKey('word.id'))
+        db.Column('word_id', db.Integer, db.ForeignKey('word.id')),
+        db.Column('url_id', db.Integer, db.ForeignKey('url.id'))
 )
+
+# user []
+# link []
+# words [][][][][][][][]
+
+class Word(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    english = db.Column(db.String(20))
+    mean = db.Column(db.String(20))
+
+    def __init__(self, english, mean):
+        self.english = english
+        self.mean = mean
+
+class ReferUrl(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word_book_id = db.Column(db.Integer, db.ForeignKey('word_book.id'))
+    url_id = db.Column(db.Integer, db.ForeignKey('url.id'))
 
 class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    recentUrl = db.Column(db.String(100))
-    words = db.relationship('Word', secondary=words, backref=db.backref('words', lazy='dynamic'))
+    link = db.Column(db.String(120))
+    words = db.relationship('Word', secondary=words,
+        backref=db.backref('urls', lazy='dynamic'))
 
-class WordInfo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(100))
-    mean = db.Column(db.String(100))
-    deleteCheck = db.Column(db.Boolean) # default = N
+    def __init__(self, link):
+        self.link = link
+
+#insert english, mean in Word table
+def insertWordData(english, mean):
+
+    wordInfo = Word(english, mean)
+
+    try:
+        db.session.add(wordInfo)
+        # db.session.add(wordBookInput)
+        # db.session.add(urlInput)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+
+    # users = Word.query.all()
+    # admin = User.query.filter_by(username='haha').all()
+    # print users
+
+    print '============================'            
+
+
+#insert account in User table
+def insertUserData(email):
+    print email
+    
+    userInfo = User(email)
+
+
+    try:
+        db.session.add(userInfo)
+        # db.session.add(wordBookInput)
+        # db.session.add(urlInput)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+
+    # users = User.query.all()
+    # admin = User.query.filter_by(username='haha').all()
+    # print users
+
+    print '============================'            
