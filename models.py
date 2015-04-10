638aa7cbf0113898a61f8c1ea095c302217e1833
dev_brian_cho <kwang8992@gmail.com>
2015-04-11 08:51:39 +0900

    DB insert  기능 구현 완료!

--- models.py
+++ models.py
@@ -16,28 +16,6 @@ words = db.Table('words',
         db.Column('url_id', db.Integer, db.ForeignKey('url.id'))
 )
 
-def renewWords(english, link):
-    linkInfo = Url.findByLink(link)
-    wordInfo = Word.findByWord(english)
-
-    linkInfo.words.append(wordInfo)
-    db.session.add(linkInfo)
-    db.session.commit()
-
-    print wordInfo.id
-    print linkInfo.id
-
-def renewUrls(email, link):
-    userInfo = User.findByEmail(email)
-    linkInfo = Url.findByLink(link)
-
-    userInfo.urls.append(linkInfo)
-    db.session.add(userInfo)
-    db.session.commit()
-
-    print userInfo.id
-    print linkInfo.id
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(45))
@@ -53,48 +31,32 @@ class User(db.Model):
 
     def make_relationship_url(self, url):
         if not url in self.urls:
-            self.urls.append(linkInfo)
+            self.urls.append(url)
             db.session.add(self)
             db.session.commit()
 
     @classmethod
-    def insertUser(cls, email):
+    def get(cls, email):
+        user = cls.find_by_email(email)
+        if user is None:
+            cls.insert_email(email)
+        inserted_user = cls.find_by_email(email)
+        return inserted_user
+
+    @classmethod
+    def insert_email(cls, email):
         u = User(email)
         db.session.add(u)
         db.session.commit()
 
     @classmethod
-    def findByEmail(cls, email):
+    def find_by_email(cls, email):
         user = cls.query.filter_by(email=email).first()
         return user
 
-# user []
-# link []
-# words [][][][][][][][]
-
-class Word(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    english = db.Column(db.String(20))
-    mean = db.Column(db.String(20))
-
-    def __init__(cls, english, mean):
-        cls.english = english
-        cls.mean = mean
-
-    @classmethod
-    def insertWord(cls, english, mean):
-        w = Word(english, mean)
-        db.session.add(w)
-        db.session.commit()
-
-    @classmethod
-    def findByWord(cls, english):
-        word = cls.query.filter_by(english=english).first()
-        return word
-
 class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(120))
+    link = db.Column(db.String(200))
     words = db.relationship('Word', secondary=words,
         backref=db.backref('urls', lazy='dynamic'))
 
@@ -108,55 +70,101 @@ class Url(db.Model):
             db.session.commit()
 
     @classmethod
-    def insertLink(cls, link):
+    def get(cls, url):
+        link = cls.find_by_link(url)
+        if link is None:
+            cls.insert_link(url)
+        inserted_link = cls.find_by_link(url)
+        return inserted_link
+
+    @classmethod
+    def insert_link(cls, link):
         l = Url(link)
         db.session.add(l)
         db.session.commit()
 
     @classmethod
-    def findByLink(cls, link):
+    def find_by_link(cls, link):
         link = cls.query.filter_by(link=link).first()
         return link
 
+class Word(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    english = db.Column(db.String(20))
+    mean = db.Column(db.String(20))
+
+    def __init__(cls, english, mean):
+        cls.english = english
+        cls.mean = mean
+
+    @classmethod
+    def get(cls, word):
+        english = word.get('english')
+        mean = word.get('mean')
+        word = cls.find_by_word(english)
+        if word is None:
+            cls.insert_word(english, mean)
+        inserted_word = cls.find_by_word(english)
+        return inserted_word
+
+    @classmethod
+    def insert_word(cls, english, mean):
+        w = Word(english, mean)
+        db.session.add(w)
+        db.session.commit()
+
+    @classmethod
+    def find_by_word(cls, english):
+        word = cls.query.filter_by(english=english).first()
+        return word
 
 class ReferUrl(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     word_book_id = db.Column(db.Integer, db.ForeignKey('word_book.id'))
     url_id = db.Column(db.Integer, db.ForeignKey('url.id'))
 
-    def __init__(cls, wordBookId, linkId):
-        cls.wordBookId = wordBookId
-        cls.linkId = linkId
+    def __init__(cls, word_book_id, url_id):
+        cls.word_book_id = word_book_id
+        cls.url_id = url_id
 
     @classmethod
-    def renewReferUrl(cls, wordBookId, linkId):
-        ru = ReferUrl(wordBookId, linkId)
-        db.session.add(ru)
+    def get(cls, wordbook, url):
+        refer_url = cls.find_by_wordbook_url(wordbook, url)
+        if refer_url is None:
+            cls.insert_refer_url(wordbook, url)
+        refer_url = cls.find_by_wordbook_url(wordbook, url)
+        return refer_url
+
+    @classmethod
+    def insert_refer_url(cls, wordbook, url):
+        refer_url = ReferUrl(wordbook.id, url.id)
+        db.session.add(refer_url)
         db.session.commit()
-    # @classmethod
-    # def findByReferUrl(cls, linkId):
+
+    @classmethod
+    def find_by_wordbook_url(cls, wordbook, url):
+        refer_url = cls.query.filter_by(word_book_id=wordbook.id, url_id=url.id).first()
+        return refer_url
+
 
 class WordBook(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     word_id = db.Column(db.Integer, db.ForeignKey('word.id'))
+    deleteCheck = db.Column(db.String(2)
     refer_urls = db.relationship('ReferUrl', backref='word_book',
                                 lazy='dynamic')
     
-    def __init__(cls, userId, wordId):
-        cls.userId = userId
-        cls.wordId = wordId
+    def __init__(cls, user_id, word_id):
+        cls.user_id = user_id
+        cls.word_id = word_id
 
     def make_relationship_referurl(self, url):
         refer_url = ReferUrl.get(self, url)
         if not refer_url in self.refer_urls:
             self.refer_urls.append(refer_url)
-
-    @classmethod
-    def renewWordBook(cls, userId, wordId):
-        wb = WordBook(userId, wordId)
-        db.session.add(wb)
-        db.session.commit()
+            db.session.add(self)
+            db.session.commit()
 
     @classmethod
     def find_by_user_word(cls, user, word):
@@ -164,26 +172,26 @@ class WordBook(db.Model):
         return wb
 
     @classmethod
+    def insert_wordbook(cls, user_id, word_id):
+        wb = WordBook(user_id, word_id)
+        db.session.add(wb)
+        db.session.commit()
+
+    @classmethod
     def get(cls, user, word):
         word_book = WordBook.find_by_user_word(user, word)
         if word_book is None:
-            WordBook.renewWordBook(user.id, word.id)
+            WordBook.insert_wordbook(user.id, word.id)
         wb = WordBook.find_by_user_word(user, word)
         return wb
 
 def searchWord(email, link, word):
-    # userCheck = User.findByEmail(email)
-    # linkCheck = Url.findByLink(link)
-    # wordCheck = Word.findByWord(word)
-
-    # print userCheck.id
-    # print linkCheck.id
-    # print wordCheck.id
 
     # user가 있나 없나 체크.
-    user_email = "hello@world.com"
-    user_url = "http://www.google.com"
-    user_words = [word1, word2]
+    user_email = email
+    user_url = link
+    # user_words = [{'hello':'a'}, {'bye':'b'}]
+    user_words = word
 
     #User.get()은 항상 User를 반환한다. (유저가 있으면 넣고 없으면 생성한후 반환)
     user = User.get(user_email)
@@ -192,42 +200,11 @@ def searchWord(email, link, word):
     user.make_relationship_url(url)
 
     for user_word in user_words:
-        word = Word.get(word)
+        word = Word.get(user_word)
         url.make_relationship_word(word)
 
         word_book = WordBook.get(user, word)
         word_book.make_relationship_referurl(url)
-
-
-    # if userCheck is None:
-    #     print 'email none'
-    #     if linkCheck is None and wordCheck is None:
-    #         print 'hasnot link, hasnot word'
-    #         #4
-    #     elif linkCheck is not None and wordCheck is None:
-    #         print 'has link, hasnot word'
-    #         # 3
-    #     elif linkCheck is None and wordCheck is not None:
-    #         print 'hasnot link, has word'
-    #         # 2
-    #     else:
-    #         print 'has link, has word'
-    #         # 1
-    # else:
-    #     print 'has email'
-    #     if linkCheck is None and wordCheck is None:
-    #         print 'hasnot link, hasnot word'
-    #         #4
-    #     elif linkCheck is not None and wordCheck is None:
-    #         print 'has link, hasnot word'
-    #         #3
-    #     elif linkCheck is None and wordCheck is not None:
-    #         print 'hasnot link, has word'
-    #         #2
-    #     else:
-    #         print 'has link, has word'
-    #         #1
-
 
 def testData():
     email1 = 'kwanggoo@gmail.com'
@@ -260,18 +237,10 @@ def testData():
     # case8 = {'email':email2, 'link':link4, 'english':eng4, 'mean':mean4}
 
     case1 = {'email':email1, 'link':link1, 'words':[word1, word2]}
-    case2 = {'email':email1, 'link':link2, 'words':[word1, word2]}
-    case3 = {'email':email1, 'link':link1, 'words':[word1, word2]}
-    case4 = {'email':email1, 'link':link2, 'words':[word1, word2]}
+    case2 = {'email':email1, 'link':link2, 'words':[word2, word3]}
+    case3 = {'email':email1, 'link':link1, 'words':[word3, word4]}
+    case4 = {'email':email1, 'link':link2, 'words':[word4, word1]}
     case5 = {'email':email2, 'link':link3, 'words':[word1, word2]}
-    case6 = {'email':email2, 'link':link4, 'words':[word1, word2]}
-    case7 = {'email':email2, 'link':link3, 'words':[word1, word2]}
-    case8 = {'email':email2, 'link':link4, 'words':[word1, word2]}
-
-
-
-
-
-
-
-
+    case6 = {'email':email2, 'link':link4, 'words':[word2, word3]}
+    case7 = {'email':email2, 'link':link3, 'words':[word3, word4]}
+    case8 = {'email':email2, 'link':link4, 'words':[word4, word1]}
