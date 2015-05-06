a89f044d6924c3311de391c64b1d9a35d96150f1
dev_brian_cho <kwang8992@gmail.com>
2015-05-06 09:11:41 +0900

    단어에 물려있는 url의 갯수에 따라 내림차순 정렬을 하는 메서드 제

--- module.py
+++ module.py
@@ -57,3 +57,136 @@ def translateWords(data):
 		# if keyCount == len(a):
 		# 	print 'complete'
 	return wordList
+
+#quick Sort
+def quickSort(alist):
+
+   quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+   if first<last:
+
+       splitpoint = partition(alist,first,last)
+
+       quickSortHelper(alist,first,splitpoint-1)
+       quickSortHelper(alist,splitpoint+1,last)
+
+def partition(alist,first,last):
+   pivotvalue = alist[first].get('urls')
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and alist[leftmark].get('urls') >= pivotvalue:
+           leftmark = leftmark + 1
+
+       while alist[rightmark].get('urls') <= pivotvalue and rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = alist[leftmark]
+           alist[leftmark] = alist[rightmark]
+           alist[rightmark] = temp
+
+   temp = alist[first]
+   alist[first] = alist[rightmark]
+   alist[rightmark] = temp
+
+
+   return rightmark
+
+
+def dbException():
+failed=False
+try:
+    db.session.commit()
+except Exception as e:
+    db.session.rollback()
+    db.session.flush()
+    failed=True
+
+def insert_data(email, link, words):
+    app.logger.info(email)
+    app.logger.info(link)
+    app.logger.info(len(words))
+    user = User.get(email)
+    url = Url.get(link)
+
+    user.make_relationship_url(url)
+
+    for user_word in words:
+        english = user_word.get('english')
+        mean = user_word.get('mean')
+        mean = ','.join(mean)
+        word = Word.get(english, mean)
+        url.make_relationship_word(word)
+
+        word_book = WordBook.get(user, word)
+        word_book.make_relationship_referurl(url)
+
+def select_word_for_web(email, link):
+    user = User.get(email)
+    url = Url.get(link)
+
+    deleted_word_list = []
+    word_list = []
+
+    for word in url.words:
+        wb = WordBook.query.filter_by(user_id=user.id, word_id=word.id).first()
+        w = Word.query.filter_by(id=wb.word_id).first()
+        english = w.english
+        mean = w.mean.split(',')
+
+        words = {'english': english, 'mean': mean, 'urls':len(wb.refer_urls.all())}
+        if wb.is_deleted:
+            deleted_word_list.append(words)
+        else:
+            word_list.append(words)
+    quickSort(word_list)
+    word_list = json.dumps(word_list)        
+    return word_list
+
+def select_word_for_mobile(email):
+    user = User.get(email)
+
+    word_list = []
+    for word in user.word_books:
+        w = Word.query.filter_by(id=word.id).first()
+        english = w.english
+        mean = w.mean
+
+        words = {'english': english, 'mean': mean}
+        if not word.is_deleted:
+            word_list.append(words)
+    word_list = json.dumps(word_list)
+    return word_list
+
+def select_delete_word_for_mobile(email):
+    user = User.get(email)
+
+    deleted_word_list = []
+    for word in user.word_books:
+        w = Word.query.filter_by(id=word.id).first()
+        english = w.english
+        mean = w.mean
+
+        words = {'english': english, 'mean': mean}
+        if word.is_deleted:
+            deleted_word_list.append(words)
+    deleted_word_list = json.dumps(deleted_word_list)
+    return deleted_word_list
+
+def find_user_info(email):
+    user = User.get(email)
+
+    all_word_count = len(user.word_books.all())
+    deleted_word_count = len(user.word_books.filter_by(is_deleted=True).all())
+    url_count = len(user.urls)
+
+    count = {'all_word_count': all_word_count, 'deleted_word_count': deleted_word_count, 'url_count': url_count}
+    return json.dumps(count)
