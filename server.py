25418d36f2a4ea60eba848d0be072f50b988cb03
dev_brian_cho <kwang8992@gmail.com>
2015-07-09 01:18:57 +0900

    same origin policy 회피를 위하여 CORS 적용.

--- server.py
+++ server.py
@@ -100,6 +100,7 @@ def get_for_userInfo():
 	return data
 
 @app.route('/allWords', methods=['GET'])
+@cross_origin() # allow all origins all methods.
 def get_all_words():
 	data = find_all_words()
 	
